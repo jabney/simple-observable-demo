b2fabc6bc774a4cf9cbd46900e9e78ce9fe38a97
James Abney <james.abney@gmail.com>
2017-07-17 09:19:53 -0700

    publicPath change

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -30,7 +30,7 @@ module.exports = webpackMerge(common.client, {
   output: {
     filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/simple-subject/dist/'
+    publicPath: '/simple-subject-demo/dist/'
   },
 
   module: {
