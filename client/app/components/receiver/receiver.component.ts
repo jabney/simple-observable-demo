6c88b960300db8adbc1a72acedd50938d393c90f
James Abney <james.abney@gmail.com>
2017-07-17 13:21:33 -0700

    Declare ReceiverComponent

---
+++ client/app/components/receiver/receiver.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'receiver',
+  templateUrl: 'receiver.component.html',
+  styleUrls: ['receiver.component.scss']
+})
+
+export class ReceiverComponent implements OnInit {
+  constructor() { }
+
+  public ngOnInit() { }
+}
