f5dda7b26f65d1b78ed71e1d764074c41ea10de7
James Abney <james.abney@gmail.com>
2017-07-17 15:35:31 -0700

    Implement ReceiverComponent logic

--- client/app/components/receiver/receiver.component.ts
+++ client/app/components/receiver/receiver.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 
 @Component({
   selector: 'receiver',
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class ReceiverComponent implements OnInit {
+  private subscribed: boolean
+
+  @Output() private subscribeAction = new EventEmitter<number>()
+
   constructor() { }
+
+  public subscribe() {
+    this.subscribed = true
+    this.subscribeAction.emit(1)
+  }
+
+  public unsubscribe() {
+    this.subscribed = false
+    this.subscribeAction.emit(-1)
+  }
+
+  public isSubscribed() {
+    return this.subscribed
+  }
+
+  public toggle() {
+    if (this.isSubscribed()) {
+      this.unsubscribe()
+    } else {
+      this.subscribe()
+    }
+  }
+
+  public buttonText() {
+    return this.isSubscribed() ? 'Unsubscribe' : 'Subscribe'
+  }
 
   public ngOnInit() { }
 }
