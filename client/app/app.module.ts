324c689c67d0317173694a41c0e2773e8afe5850
James Abney <james.abney@gmail.com>
2017-07-17 12:17:53 -0700

    Import and configure SenderComponent

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -6,10 +6,11 @@ import { APP_MESSAGES } from './tokens'
 import { MessageService } from './services/message.service'
 
 import { AppComponent } from './app.component'
+import { SenderComponent } from './components/sender.component'
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SenderComponent ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
